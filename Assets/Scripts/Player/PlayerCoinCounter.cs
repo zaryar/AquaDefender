40d20ac28979c65bb2638c25bd1e55e519da6197
Martin Eichler <eichlermartin37@gmail.com>
2023-11-24 22:05:45 +0100

    added sound

--- Assets/Scripts/Player/PlayerCoinCounter.cs
+++ Assets/Scripts/Player/PlayerCoinCounter.cs
@@ -5,12 +5,14 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 public class CoinCounter : MonoBehaviour
 {
     public int coinCount = 0;
+    public AudioClip coinSound;
     //public Text coinText;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
             Destroy(other.gameObject);
             coinCount++;
             //coinText.text = "Coins: " + coinCount.ToString();
