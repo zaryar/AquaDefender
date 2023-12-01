6a5c6185b1c4102d3005cf0953aebab6850845df
Martin Eichler <eichlermartin37@gmail.com>
2023-12-01 19:28:45 +0100

    removed 2 usings

--- Assets/Scripts/Player/PlayerCoinCounter.cs
+++ Assets/Scripts/Player/PlayerCoinCounter.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
+//using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class CoinCounter : MonoBehaviour
 {
