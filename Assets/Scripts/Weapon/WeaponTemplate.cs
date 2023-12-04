615123f37974db8e7ce5d3779c815869ef32db54
VBieri <valentin.biermaier@gmx.de>
2023-12-04 20:12:01 +0100

    Relate load of attackTransform to gameObject Before the attackTransform was loaded with generic GameObject.Find('Muzzle'), but there are many 'Muzzles'. So the load is related to the Gun and Sword that are part of the gameObject that instantiated the class.

--- Assets/Scripts/Weapon/WeaponTemplate.cs
+++ Assets/Scripts/Weapon/WeaponTemplate.cs
@@ -20,6 +20,16 @@ public class WeaponTemplate : MonoBehaviour {
      protected virtual void Awake()
     {
         attackTransform = GameObject.Find("Muzzle").transform;
+        if (gameObject.name.Contains("Gun"))
+        {
+            attackTransform = gameObject.transform.Find("Muzzle");
+
+        }
+        else if (gameObject.name.Contains("Sword"))
+        {
+            attackTransform = gameObject.transform.parent.transform;
+        }
+        
         checkOpposingFraction();
        
     }
