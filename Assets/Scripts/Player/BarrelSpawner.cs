44e0e96de2d09fe160960a20334d46b249259f94
Triblecraft <tmerlin@gmx.de>
2023-12-04 15:49:54 +0100

    new Barralspawning
    barrlas can not spawn in other objects

--- Assets/Scripts/Player/BarrelSpawner.cs
+++ Assets/Scripts/Player/BarrelSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class BarrelSpawner : MonoBehaviour
@@ -7,19 +8,18 @@ public class BarrelSpawner : MonoBehaviour
     Transform barrelSpawnPoint;
     [SerializeField] GameObject barrel;
     [SerializeField] float reloadBarrel = 40f;
+    [SerializeField] Vector3 spawnCheckSize = new Vector3(0.1f, 0.1f, 0.1f); // Größe der Überprüfungsbox
 
     bool reloading = false;
-
 
     private void Awake()
     {
         barrelSpawnPoint = GameObject.Find("BarrelSpawn").transform;
-
     }
 
     public void SpawnBarrel()
     {
-        if (!reloading)
+        if (!reloading && CanSpawnBarrel())
         {
             Instantiate(barrel, transform.position, barrelSpawnPoint.rotation);
             reloading = true;
@@ -28,6 +28,20 @@ public class BarrelSpawner : MonoBehaviour
     }
 
 
+    bool CanSpawnBarrel()
+    {
+        Collider[] hitColliders = Physics.OverlapBox(barrelSpawnPoint.position, spawnCheckSize / 2, Quaternion.identity);
+        foreach (var hitCollider in hitColliders)
+        {
+
+            if (hitCollider.gameObject != gameObject && hitCollider.tag != "Player")
+            {
+
+                return false;
+            }
+        }
+        return true;
+    }
 
     private IEnumerator Cooldown(float time)
     {
