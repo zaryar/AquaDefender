14f4f3885456df5e9786acef77cbc76581d3d457
KozlowskiXY <frieder.wallner@web.de>
2023-12-17 03:18:51 +0100

    add swapping for sword models
    The player now holds 3 different sword models, the active model can be set through the SwordModelSwapper without messing directly with the mesh renderers. The Script is usable for Enemies as well.

---
+++ Assets/Scripts/Weapon/WeaponSwapping/SwordModelSwapper.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordModelSwapper : MonoBehaviour
+{
+    [SerializeField] MeshRenderer Sword1;
+    [SerializeField] MeshRenderer Sword2;
+    [SerializeField] MeshRenderer Sword3;
+
+    Weapons.Swords _currentSword = Weapons.Swords.sword1;
+    private bool _isActive = false;
+
+    public void swapModel(Weapons.Swords sword)
+    {
+        _currentSword= sword;
+        if(_isActive)
+        {
+            disableAll();
+            switch (sword)
+            {
+                case Weapons.Swords.sword1:
+                    Sword1.enabled = true;
+                    break;
+                case Weapons.Swords.sword2:
+                    Sword2.enabled = true;
+                    break;
+                case Weapons.Swords.sword3:
+                    Sword3.enabled = true;
+                    break;
+            }
+        }  
+    }
+
+    public void enableRenderer()
+    {
+        _isActive = true;
+        swapModel(_currentSword);
+    }
+
+    public void disableRenderer()
+    {
+        disableAll();
+        _isActive = false;
+    }
+
+    private void Start()
+    {
+        if(Sword1 == null ||
+           Sword2 == null ||
+           Sword3 == null) {
+            Debug.LogError("SwordModelSwapper is missing Links in the inspector.");
+        }
+        disableAll();
+    }
+
+    private void disableAll()
+    {
+        Sword1.enabled = false;
+        Sword2.enabled = false;
+        Sword3.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponSwapping/Weapons.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons
+{
+    public enum Swords {
+        sword1, sword2, sword3
+    }
+}
