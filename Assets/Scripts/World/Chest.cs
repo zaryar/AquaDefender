8af6dc12adf398c55847e50eab3d0cad067b2e47
JuliaSchneider01 <julia14.schneider@t-online.de>
2024-02-24 23:43:06 +0100

    bugfix

--- Assets/Scripts/World/Chest.cs
+++ Assets/Scripts/World/Chest.cs
@@ -4,35 +4,39 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 public class Chest : MonoBehaviour
-{   
-    [SerializeField]bool isOpen = false;
+{
+    [SerializeField] bool isOpen = false;
     GameObject player;
     private Animator animator;
     private AudioSource audioSource;
-    public AudioClip chestOpen; 
+    // public AudioClip chestOpen; 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
-        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public void openChest() {
-        animator.SetBool("isOpen", true);
-        PlayerMovementController playerMovementController = player.GetComponent<PlayerMovementController>();
-        playerMovementController.gotInvisibility = true;
+    public void openChest()
+    {
 
-        if (chestOpen != null && audioSource != null)
-                {
-                    audioSource.PlayOneShot(chestOpen);
-                }
+        if (!isOpen)
+        {
+            Debug.Log(audioSource, audioSource.clip);
+            audioSource.Play();
 
+            animator.SetBool("isOpen", true);
+            PlayerMovementController playerMovementController = player.GetComponent<PlayerMovementController>();
+            playerMovementController.gotInvisibility = true;
+        }
+        isOpen = true;
     }
 
-    public void closeChest() {
+    public void closeChest()
+    {
         animator.SetBool("isOpen", false);
     }
 
-    
+
 }
