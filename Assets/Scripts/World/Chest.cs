d898876830a90cc8700f274ced7c5f778a4ab7b1
JuliaSchneider01 <julia14.schneider@t-online.de>
2024-02-24 22:50:00 +0100

    AddSound

--- Assets/Scripts/World/Chest.cs
+++ Assets/Scripts/World/Chest.cs
@@ -8,20 +8,31 @@ public class Chest : MonoBehaviour
     [SerializeField]bool isOpen = false;
     GameObject player;
     private Animator animator;
+    private AudioSource audioSource;
+    public AudioClip chestOpen; 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     public void openChest() {
         animator.SetBool("isOpen", true);
         PlayerMovementController playerMovementController = player.GetComponent<PlayerMovementController>();
         playerMovementController.gotInvisibility = true;
+
+        if (chestOpen != null && audioSource != null)
+                {
+                    audioSource.PlayOneShot(chestOpen);
+                }
+
     }
 
     public void closeChest() {
         animator.SetBool("isOpen", false);
     }
+
+    
 }
