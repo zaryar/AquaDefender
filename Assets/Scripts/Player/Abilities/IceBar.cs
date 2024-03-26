681aa699bf9611ad607d7ad2c398b2f24b3c71d0
JuliaSchneider01 <julia14.schneider@t-online.de>
2024-03-26 17:18:58 +0100

    automate access

--- Assets/Scripts/Player/Abilities/IceBar.cs
+++ Assets/Scripts/Player/Abilities/IceBar.cs
@@ -3,17 +3,21 @@ using UnityEngine.UI;
 
 public class IceBar : MonoBehaviour
 {
-    public Slider energySlider; 
-    public Graphic Ice;
-    public Color lightBlue;
-    public Graphic Snowflake;
+    [HideInInspector] public Slider energySlider; 
+    [HideInInspector] public Graphic Ice;
+    [HideInInspector] public Color lightBlue;
+    [HideInInspector] public Graphic Snowflake;
 
     public void Start(){
 
+        energySlider = GetComponentInChildren<Slider>();
+        Ice = transform.Find("Ice").GetComponent<Graphic>();
+        Snowflake = transform.Find("snowflake").GetComponent<Graphic>();
         lightBlue = Ice.color;
         Ice.color = Color.grey;
         Snowflake.color = Color.grey;
     }
+
 
     public void SetMaxEnergy(float energy){
 
