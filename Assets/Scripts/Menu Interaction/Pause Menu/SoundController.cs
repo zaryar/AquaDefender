f213a1c3be112bb8ca9d73625615310a4b1e04b5
Matthias Vetter <matze2948@gmail.com>
2024-03-28 21:08:52 +0100

    anpassbare Volume in settings
    from 0% to 100% and looked on the edgecases (if muted and increase volume then it unmutes and so on.)

--- Assets/Scripts/Menu Interaction/Pause Menu/SoundController.cs
+++ Assets/Scripts/Menu Interaction/Pause Menu/SoundController.cs
@@ -1,29 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SoundController : MonoBehaviour
 {
-    private bool isSoundOn = true;
     public GameObject imageObject;
+    public TextMeshProUGUI volumeText;
 
     void Start()
     {
         imageObject.SetActive(false);
+        if (volumeText != null)
+        {
+            volumeText.text = "" + Mathf.RoundToInt(AudioListener.volume * 100) + "%";
+        }
     }
 
-        public void TurnSoundOn()
+    public void TurnSoundOn()
     {
-        isSoundOn = true;
         AudioListener.pause = false; // Sound einschalten
         imageObject.SetActive(false);
+        if (Mathf.RoundToInt(AudioListener.volume * 100) == 0)
+        {
+            IncreaseVolume();
+        }
     }
 
     public void TurnSoundOff()
     {
-        isSoundOn = false;
         AudioListener.pause = true; // Sound ausschalten
         imageObject.SetActive(true);
+    }
+
+    // Funktion zum Anpassen der Lautstärke von 0% bis 100%
+    public void AdjustVolume(float volumePercent)
+    {
+        float volume = Mathf.Clamp01(volumePercent / 100f);
+
+        AudioListener.volume = volume;
+
+        UpdateVolumeText();
+    }
+
+    // Erhöhen der Lautstärke um 10%
+    public void IncreaseVolume()
+    {
+        AdjustVolume((AudioListener.volume * 100) + 10);
+        TurnSoundOn();
+    }
+
+    // Verringern der Lautstärke um 10%
+    public void DecreaseVolume()
+    {
+        AdjustVolume((AudioListener.volume * 100) - 10);
+        if (Mathf.RoundToInt(AudioListener.volume * 100) > 0)
+        {
+            TurnSoundOn();
+        } 
+        else 
+        {
+            TurnSoundOff(); 
+        }
+    }
+
+    private void UpdateVolumeText()
+    {
+        if (volumeText != null)
+        {
+            volumeText.text = "" + Mathf.RoundToInt(AudioListener.volume * 100) + "%";
+        }
     }
 }
 
