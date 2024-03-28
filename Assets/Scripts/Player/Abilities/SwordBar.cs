6782e148e0177cbea05d6bab0131c129d9de0699
JuliaSchneider01 <julia14.schneider@t-online.de>
2024-03-28 17:41:20 +0100

    Add Sounds Folder

--- Assets/Scripts/Player/Abilities/SwordBar.cs
+++ Assets/Scripts/Player/Abilities/SwordBar.cs
@@ -6,17 +6,17 @@ public class SwordBar : MonoBehaviour
     [HideInInspector] public Slider energySlider; 
     [HideInInspector] public Graphic Ice;
     [HideInInspector] public Color lightBlue;
-    [HideInInspector] public Graphic Snowflake;
+    //[HideInInspector] public Graphic Snowflake;
 
     public void Start(){
 
         energySlider = GetComponentInChildren<Slider>();
         Ice = transform.Find("Ice").GetComponent<Graphic>();
-        Snowflake = transform.Find("snowflake").GetComponent<Graphic>();
+        //Snowflake = transform.Find("Snowflake").GetComponent<Graphic>();
         
         lightBlue = Ice.color;
         Ice.color = Color.grey;
-        Snowflake.color = Color.grey;
+        //Snowflake.color = Color.grey;
     }
 
 
